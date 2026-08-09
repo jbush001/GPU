51ec6f64bdca7a7622d22a724e1bddfdb6ae02db
Jeff Bush <jeffbush001@gmail.com>
2026-08-09 16:38:22 -0700

    Implement thread scheduler

---
+++ hardware/main/shader/ThreadScheduler.scala
@@ -0,0 +1,127 @@
+//
+//   Copyright 2026 Jeff Bush
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package gpu.shader
+
+import chisel3._
+import chisel3.util._
+import gpu._
+
+/** This module select between hardware thread to issue to the pipeline. It also
+  * tracks current runnable state of each thread and handles assigning new jobs to
+  * threads.
+  */
+class ThreadScheduler(implicit val cfg: GpuConfig) extends Module {
+  val io  = IO(new Bundle {
+    // From external fixed function units.
+    val startJob = Flipped(Decoupled(new Bundle {
+      val startPc = UInt(cfg.busAddressBits.W)
+    }))
+
+    // Output to the instruction cache.
+    val nextIssue = Valid(new Bundle {
+      val threadId = UInt(log2Ceil(cfg.shaderThreads).W)
+      val pc = UInt(cfg.busAddressBits.W)
+    })
+
+    val haltRequest = Flipped(Valid(UInt(log2Up(cfg.shaderThreads).W)))
+    val wakeThreadBitmap = Input(UInt(cfg.shaderThreads.W))
+    val stallThreadBitmap = Input(UInt(cfg.shaderThreads.W))
+
+    // We an rollback a thread in response to a branch instruction or if
+    // it stalls for some reason.
+    val rollback = Flipped(Valid(new Bundle {
+      val threadId = UInt(log2Ceil(cfg.shaderThreads).W)
+      val pc = UInt(cfg.busAddressBits.W)
+    }))
+  })
+
+  val programCounters = RegInit(VecInit(Seq.fill(cfg.shaderThreads)(0.U(cfg.busAddressBits.W))))
+  val threadHalted = RegInit(VecInit(Seq.fill(cfg.shaderThreads)(true.B)))
+  val threadStalled = RegInit(VecInit(Seq.fill(cfg.shaderThreads)(false.B)))
+
+  // Threads start upon request and run to completion, halting when they
+  // reach a HALT instruction. This logic tracks which threads are active
+  // and assigns new threads on request. This unit can only start one new
+  // thread per cycle.
+  val nextFreeThread = PriorityEncoder(threadHalted.asUInt)
+  io.startJob.ready := threadHalted.asUInt.orR
+  when (io.startJob.fire) {
+    threadHalted(nextFreeThread) := false.B
+    programCounters(nextFreeThread) := io.startJob.bits.startPc
+  }
+
+  when (io.haltRequest.valid) {
+    assert(!threadHalted(io.haltRequest.bits), "Cannot halt a thread that is already halted")
+    threadHalted(io.haltRequest.bits) := true.B
+  }
+
+  // The pipeline itself never stalls, but threads can stall if they are waiting for
+  // external soruces. This manages the stall state for each thread.
+  for (i <- 0 until cfg.shaderThreads) {
+    assert(!(io.wakeThreadBitmap(i) && io.stallThreadBitmap(i)), "Cannot wake and stall a thread at the same time")
+    assert(!threadStalled(i) || !io.stallThreadBitmap(i), "Cannot stall a thread that is already stalled")
+    assert(threadStalled(i) || !io.wakeThreadBitmap(i), "Cannot wake a thread that is not stalled")
+    assert(!threadHalted(i) || !io.stallThreadBitmap(i), "Cannot stall a thread that is halted")
+
+    // TODO There is actually an edge case where this can happen: if an instruction cache miss occurs
+    // while fetching the next instruction and a previously fetched instruction is HALT, the
+    // wakeup can occur later. Need to handle this case explicitly.
+    assert(!threadHalted(i) || !io.wakeThreadBitmap(i), "Cannot wake a thread that is halted")
+
+    when (io.wakeThreadBitmap(i)) {
+      threadStalled(i) := false.B
+    }
+
+    when (io.stallThreadBitmap(i)) {
+      threadStalled(i) := true.B
+    }
+  }
+
+  // Select the thread to issue.
+  val threadIssueArbiter = Module(new RRArbiter(UInt(cfg.busAddressBits.W), cfg.shaderThreads))
+  for (i <- 0 until cfg.shaderThreads) {
+    threadIssueArbiter.io.in(i).valid := !threadHalted(i) && !threadStalled(i)
+    threadIssueArbiter.io.in(i).bits := programCounters(i)
+  }
+
+  threadIssueArbiter.io.out.ready := true.B
+
+  io.nextIssue.valid := (threadIssueArbiter.io.out.valid
+    && !(io.stallThreadBitmap(threadIssueArbiter.io.chosen)))
+  io.nextIssue.bits.threadId := threadIssueArbiter.io.chosen
+
+  // Note: the rollback signal can end up being a critical timing path. An
+  // alternative would be to set valid to false this cycle and skip issuing
+  // the instruction, but should do timing analysis to see if this is
+  // necessary.
+  val nextIssuePc = Mux(io.rollback.valid && io.rollback.bits.threadId === threadIssueArbiter.io.chosen,
+                        io.rollback.bits.pc,
+                        programCounters(threadIssueArbiter.io.chosen))
+  io.nextIssue.bits.pc := nextIssuePc
+
+  // Rollback a thread to a previous PC.
+  when (io.rollback.valid) {
+    programCounters(io.rollback.bits.threadId) := io.rollback.bits.pc
+  }
+
+  // Advance the selected program counter.
+  // NOTE: if the thread is rolled back the same cycle it is issued, this
+  // should take precendence, since it factors in the increment.
+  when (io.nextIssue.valid) {
+    programCounters(threadIssueArbiter.io.chosen) := nextIssuePc + 4.U
+  }
+}
---
+++ hardware/test/shader/ThreadSchedulerTests.scala
@@ -0,0 +1,206 @@
+//
+//   Copyright 2026 Jeff Bush
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package gpu.shader
+
+import gpu._
+
+import chisel3._
+import chisel3.simulator.scalatest.ChiselSim
+import org.scalatest.funsuite.AnyFunSuite
+
+class ThreadSchedulerTests extends AnyFunSuite with ChiselSim {
+  implicit val cfg: GpuConfig = new GpuConfig
+
+  test("ThreadScheduler single thread") {
+    simulate(new ThreadScheduler()) { dut =>
+      // Allocate a new job
+      dut.io.startJob.ready.expect(true.B)
+      dut.io.startJob.valid.poke(true.B)
+      dut.io.startJob.bits.startPc.poke(0x1000.U)
+      dut.clock.step()
+      dut.io.startJob.valid.poke(false.B)
+
+      // Record the thread that was allocated.
+      val allocatedThread = dut.io.nextIssue.bits.threadId.peek().litValue
+
+      // Expect next issue on same thread to advance by +4
+      for (i <- 0 until 5) {
+        dut.io.nextIssue.valid.expect(true.B)
+        dut.io.nextIssue.bits.threadId.expect(allocatedThread.U)
+        dut.io.nextIssue.bits.pc.expect((0x1000 + (i * 4)).U)
+        dut.clock.step()
+      }
+    }
+  }
+
+  test("ThreadScheduler rollback") {
+    simulate(new ThreadScheduler()) { dut =>
+      // Start a job at 0x2000
+      dut.io.startJob.valid.poke(true.B)
+      dut.io.startJob.bits.startPc.poke(0x2000.U)
+      dut.clock.step()
+      dut.io.startJob.valid.poke(false.B)
+
+      // Record the thread that was allocated.
+      val allocatedThread = dut.io.nextIssue.bits.threadId.peek().litValue
+
+      // Issue a few cycles
+      for (i <- 0 until 3) {
+        dut.io.nextIssue.valid.expect(true.B)
+        dut.io.nextIssue.bits.threadId.expect(allocatedThread.U)
+        dut.io.nextIssue.bits.pc.expect((0x2000 + (i * 4)).U)
+        dut.clock.step()
+      }
+
+      // Trigger a rollback for the issued thread to 0x0500
+      dut.io.rollback.valid.poke(true.B)
+      dut.io.rollback.bits.threadId.poke(allocatedThread.U)
+      dut.io.rollback.bits.pc.poke(0x0500.U)
+
+      // Ensure we're not executing from the new location
+      for (i <- 0 until 3) {
+        dut.io.nextIssue.valid.expect(true.B)
+        dut.io.nextIssue.bits.threadId.expect(allocatedThread.U)
+        dut.io.nextIssue.bits.pc.expect((0x500 + (i * 4)).U)
+        dut.clock.step()
+        dut.io.rollback.valid.poke(false.B)
+      }
+    }
+  }
+
+  test("ThreadScheduler stall/resume") {
+    simulate(new ThreadScheduler()) { dut =>
+      // Start job
+      dut.io.startJob.valid.poke(true.B)
+      dut.io.startJob.bits.startPc.poke(0x1000.U)
+      dut.clock.step()
+      dut.io.startJob.valid.poke(false.B)
+
+      // Record the thread that was allocated.
+      val allocatedThread = dut.io.nextIssue.bits.threadId.peek().litValue
+
+      // Issue a few cycles
+      for (i <- 0 until 8) {
+        dut.io.nextIssue.valid.expect(true.B)
+        dut.io.nextIssue.bits.threadId.expect(allocatedThread.U)
+        dut.io.nextIssue.bits.pc.expect((0x1000 + (i * 4)).U)
+        dut.clock.step()
+      }
+
+      // Stall Thread and roll back
+      dut.io.stallThreadBitmap.poke((1 << allocatedThread.toInt).U)
+      dut.io.rollback.valid.poke(true.B)
+      dut.io.rollback.bits.threadId.poke(allocatedThread.U)
+      dut.io.rollback.bits.pc.poke(0x100c.U)
+      dut.clock.step()
+      dut.io.stallThreadBitmap.poke(0.U)
+      dut.io.rollback.valid.poke(false.B)
+
+      // Thread should no longer issue
+      for (_ <- 0 until 3) {
+        dut.io.nextIssue.valid.expect(false.B)
+        dut.clock.step()
+      }
+
+      // Resume Thread
+      dut.io.wakeThreadBitmap.poke((1 << allocatedThread.toInt).U)
+      dut.clock.step()
+      dut.io.wakeThreadBitmap.poke(0.U)
+
+      // Should resume issuing from the rollback location.
+      for (i <- 0 until 4) {
+        dut.io.nextIssue.valid.expect(true.B)
+        dut.io.nextIssue.bits.threadId.expect(allocatedThread.U)
+        dut.io.nextIssue.bits.pc.expect((0x100c + (i * 4)).U)
+        dut.clock.step()
+      }
+    }
+  }
+
+  test("ThreadScheduler halt") {
+    simulate(new ThreadScheduler()) { dut =>
+      // Start job on Thread 0
+      dut.io.startJob.valid.poke(true.B)
+      dut.io.startJob.bits.startPc.poke(0x1000.U)
+      dut.clock.step()
+      dut.io.startJob.valid.poke(false.B)
+
+      // Record the thread that was allocated.
+      val allocatedThread = dut.io.nextIssue.bits.threadId.peek().litValue
+
+      // Issue a few cycles
+      for (i <- 0 until 3) {
+        dut.io.nextIssue.valid.expect(true.B)
+        dut.io.nextIssue.bits.threadId.expect(allocatedThread.U)
+        dut.io.nextIssue.bits.pc.expect((0x1000 + (i * 4)).U)
+        dut.clock.step()
+      }
+
+      // Halt Thread
+      dut.io.haltRequest.valid.poke(true.B)
+      dut.io.haltRequest.bits.poke(allocatedThread.U)
+      dut.clock.step()
+      dut.io.haltRequest.valid.poke(false.B)
+
+      // Thread should no longer issue
+      for (_ <- 0 until 3) {
+        dut.io.nextIssue.valid.expect(false.B)
+        dut.clock.step()
+      }
+    }
+  }
+
+  test("ThreadScheduler multiple thread") {
+    simulate(new ThreadScheduler()) { dut =>
+      // Start jobs on 3 threads
+      val numThreads = 3
+      val startPcs = Seq(0x1000.U, 0x2000.U, 0x3000.U)
+
+      for (pc <- startPcs) {
+        dut.io.startJob.ready.expect(true.B)
+        dut.io.startJob.valid.poke(true.B)
+        dut.io.startJob.bits.startPc.poke(pc)
+        dut.clock.step()
+      }
+      dut.io.startJob.valid.poke(false.B)
+
+      // Collect the sequence of issuing thread IDs over consecutive cycles
+      val issuedSequence = (0 until numThreads).map { _ =>
+        dut.io.nextIssue.valid.expect(true.B)
+        val tId = dut.io.nextIssue.bits.threadId.peek().litValue.toInt
+        dut.clock.step()
+        tId
+      }
+
+      // Verify all 3 threads issued exactly once without repeating
+      assert(issuedSequence.distinct.length == numThreads,
+             s"Expected $numThreads distinct threads, got $issuedSequence")
+
+      // Verify the round-robin cycle repeats in the same order for a few more cycles
+      for (_ <- 0 until 3) {
+        for (expectedThread <- issuedSequence) {
+          dut.io.nextIssue.valid.expect(true.B)
+          dut.io.nextIssue.bits.threadId.expect(expectedThread.U)
+          dut.clock.step()
+        }
+      }
+    }
+  }
+}
+
+
+
