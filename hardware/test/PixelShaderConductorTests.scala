4f47f0aa210d1502fccdb2eb1691d71db732c63c
Jeff Bush <jeffbush001@gmail.com>
2026-08-30 10:24:13 -0700

    Add PixelShaderConductor stress test.

--- hardware/test/PixelShaderConductorTests.scala
+++ hardware/test/PixelShaderConductorTests.scala
@@ -175,5 +175,93 @@ class PixelShaderConductorTests extends AnyFunSuite with ChiselSim {
       dut.io.idle.expect(true)
     }
   }
+
+  // Checks that job states are handled properly.
+  // TODO: need to perform register accesses to ensure writes
+  // and reads are propagaged.
+  test("PixelShaderConductor stress") {
+    simulate(new PixelShaderConductor) { dut =>
+      val rng = new scala.util.Random
+
+      var tilex = 1
+      var tiley = 1
+      val WIDTH = 100
+
+      class ActiveJob(val tag: Int) {
+        var cyclesLeft = rng.nextInt(20) + 10
+      }
+
+      val activeJobs = scala.collection.mutable.Set[ActiveJob]()
+      val outstandingQuads = scala.collection.mutable.Set[(Int, Int)]()
+
+      val maxCycles = 1000
+      val flushCycles = 100
+      for (cycle <- 0 until maxCycles) {
+        val flush = cycle >= maxCycles - flushCycles
+        if (flush) {
+          dut.io.flush.poke(true)
+        }
+
+        // Check on loading new rasterized quads
+        if (cycle < maxCycles - flushCycles &&
+          dut.io.rasterizedQuad.ready.peek().litToBoolean) {
+          tilex += 1
+          if (tilex >= WIDTH) {
+            tilex = 0
+            tiley += 1
+          }
+
+          dut.io.rasterizedQuad.valid.poke(true)
+          dut.io.rasterizedQuad.bits.location.x.poke(tilex)
+          dut.io.rasterizedQuad.bits.location.y.poke(tiley)
+          dut.io.rasterizedQuad.bits.mask.poke(15)
+          outstandingQuads += ((tilex, tiley))
+        } else {
+          dut.io.rasterizedQuad.valid.poke(false)
+        }
+
+        for (job <- activeJobs) {
+          if (job.cyclesLeft > 0) {
+            job.cyclesLeft -= 1
+          }
+        }
+
+        // Pick one completed job to finish
+        val completedJobs = activeJobs.filter(_.cyclesLeft == 0)
+        if (completedJobs.nonEmpty) {
+          val jobToFinish = completedJobs.head
+          activeJobs -= jobToFinish
+          dut.io.jobFinished.valid.poke(true)
+          dut.io.jobFinished.bits.poke(jobToFinish.tag)
+        } else {
+          dut.io.jobFinished.valid.poke(false)
+        }
+
+        // Assert ready if there are fewer than 8 active jobs
+        val ready = activeJobs.size < 8
+        dut.io.startJob.ready.poke(ready)
+
+        // Note we do this after checking to end jobs so we don't try to finish
+        // jobs the same cycle they start.
+        if (dut.io.startJob.valid.peek().litToBoolean && ready) {
+          val tag = dut.io.startJob.bits.tag.peek().litValue.toInt
+          activeJobs += new ActiveJob(tag)
+        }
+
+        // Handle output pixels
+        if (dut.io.shadedQuad.valid.peek().litToBoolean) {
+          val locationX = dut.io.shadedQuad.bits.location.x.peek().litValue.toInt
+          val locationY = dut.io.shadedQuad.bits.location.y.peek().litValue.toInt
+          outstandingQuads -= ((locationX, locationY))
+        }
+
+        // We ONLY step the clock here. ONLY ONLY ONLY
+        dut.clock.step()
+      }
+
+      assert(outstandingQuads.isEmpty, "There are still outstanding quads at the end of the simulation.")
+      assert(activeJobs.isEmpty, "There are still active jobs at the end of the simulation.")
+    }
+  }
 }
 
