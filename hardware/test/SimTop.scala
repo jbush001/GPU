07654baef43a0eb73c2f2e781d70f7fb138e1fe5
Jeff Bush <jeffbush001@gmail.com>
2026-09-07 07:42:29 -0700

    Optimize two instructions from texture shader.

--- hardware/test/SimTop.scala
+++ hardware/test/SimTop.scala
@@ -68,25 +68,22 @@ object Simulation extends App {
 
     val asm = new ShaderAssembler()
     asm
-      // Compute s, save in v0
+      // Compute s
       .move(64, 96) // lambda0
       .rInst(OpCode.Mulf, 64, 98, 64) // dQ1 * lambda0
       .move(66, 97) // lambda1
       .rInst(OpCode.Mulf, 66, 98, 66) // dQ2 * lambda1
       .rInst(OpCode.Addf, 64, 64, 66) // (dQ1 * lambda0) + (dQ2 * lambda1)
-      .rInst(OpCode.Addf, 64, 98, 64) // (dQ1 * lambda0) + (dQ2 * lambda1) + Q0
+      .rInst(OpCode.Addf, 108, 98, 64) // (dQ1 * lambda0) + (dQ2 * lambda1) + Q0
 
-      // Compute t, save in v1
+      // Compute t
+      // The last instruction will kick off the texture fetch
       .move(65, 96) // lambda0
       .rInst(OpCode.Mulf, 65, 98, 65) // dQ1 * lambda0
       .move(66, 97) // lambda1
       .rInst(OpCode.Mulf, 66, 98, 66) // dQ2 * lambda1
       .rInst(OpCode.Addf, 65, 65, 66) // (dQ1 * lambda0) + (dQ2 * lambda1)
-      .rInst(OpCode.Addf, 65, 98, 65) // (dQ1 * lambda0) + (dQ2 * lambda1) + Q0
-
-      // Start texture fetch
-      .move(108, 64) // s
-      .move(109, 65) // t
+      .rInst(OpCode.Addf, 109, 98, 65) // (dQ1 * lambda0) + (dQ2 * lambda1) + Q0
 
       // Read back texture data, store in output registers
       .move(104, 99) // red
